b103043e8a3d464d5da6938c82ad0ae7b0521711
LuizSuliman <luiz.suliman@sptech.school>
2023-10-23 02:12:24 -0300

    Projeto individual - Notifiações Windows

---
+++ src/main/java/TesteLuiz.java
@@ -0,0 +1,110 @@
+import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.janelas.Janela;
+import com.github.britooo.looca.api.group.processos.Processo;
+import dao.ComponenteDao;
+import dao.ComputadorDao;
+import dao.UsuarioDao;
+import modelo.Componente;
+import modelo.Computador;
+import modelo.Usuario;
+import oshi.SystemInfo;
+import servico.HowlzLuiz;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TesteLuiz {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HowlzLuiz howlz = new HowlzLuiz();
+        Looca looca = new Looca();
+        SystemInfo si = new SystemInfo();
+        UsuarioDao usuarioDao = new UsuarioDao();
+        ComputadorDao computadorDao = new ComputadorDao();
+        ComponenteDao componenteDao = new ComponenteDao();
+
+        String continuarOuNao = "";
+        Boolean cadastroValidado = false;
+        Usuario usuarioLogado = null;
+
+        do {
+            System.out.println("Digite seu email:");
+            String email = in.next();
+
+            System.out.println("Digite sua senha:");
+            String senha = in.next();
+
+            cadastroValidado = howlz.validarLogin(email, senha);
+            if (cadastroValidado) {
+                usuarioLogado = usuarioDao.buscarPeloEmailESenha(email, senha);
+                System.out.println("Bem-vindo %s!".formatted(usuarioLogado.getNome()));
+            } else {
+                System.out.println("Seu email ou senha estão incorretos!");
+                System.out.println("Deseja tentar logar novamente? S/N");
+                continuarOuNao = in.next();
+                if (continuarOuNao.equalsIgnoreCase("N")) {
+                    System.out.println("Até a próxima!");
+                    System.exit(0);
+                }
+            }
+        } while(!cadastroValidado);
+
+        if (howlz.computadorNaoCadastrado(si.getHardware().getComputerSystem().getSerialNumber())) {
+            System.out.println("Este computador ainda não está na nossa base de dados.\nQual o código que sua empresa usa para identificá-lo?");
+            String codigo = in.next();
+            howlz.cadastrarNovoComputador(usuarioLogado.getNome(), codigo);
+            howlz.cadastrarNovosComponentes(si.getHardware().getComputerSystem().getSerialNumber());
+        }
+
+        try {
+            howlz.exibirNaBandeja();
+        } catch (AWTException e) {
+            System.out.println("Monitoramento iniciado!");
+        }
+
+        Computador computador = computadorDao.buscarPeloSerial(si.getHardware().getComputerSystem().getSerialNumber());
+        List<Componente> componentes = componenteDao.buscarTodosPeloIdComputador(computador.getIdComputador());
+
+        // Agendamendo tarefa de monitoramento:
+        Timer timer = new Timer();
+        int delay = 0; // Atraso inicial
+        int intervalo = 15000; // Intervalo de tempo em milissegundos
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                for (Componente componente : componentes) {
+                    howlz.monitorarComponentes(componente);
+                }
+
+                List<String> alertas = howlz.estadoCritico();
+                if (!alertas.isEmpty()) {
+                    List<Processo> processos = looca.getGrupoDeProcessos().getProcessos();
+                    for (Processo processo : processos) {
+                        howlz.monitorarProcessos(processo, computador.getIdComputador());
+                    }
+
+                    if (SystemTray.isSupported()) {
+                        try {
+                            howlz.emitirNotificacao(alertas);
+                        } catch (AWTException e) {
+                            System.out.println("Não foi possível criar a notificação: " + e);
+                        }
+                    } else {
+                        System.out.println("Este dispositivo não tem suporte à tecnologia 'SystemTray'!");
+                    }
+                }
+
+                List<Janela> janelas = looca.getGrupoDeJanelas().getJanelasVisiveis();
+                for (Janela janela : janelas) {
+                    howlz.monitorarJanelas(janela, computador.getIdComputador(), !alertas.isEmpty());
+                }
+
+                System.out.println("Fim do Timer");
+            }
+        }, delay, intervalo);
+    }
+}
--- src/main/java/servico/HowlzLuiz.java
+++ src/main/java/servico/HowlzLuiz.java
@@ -5,6 +5,7 @@ import com.github.britooo.looca.api.group.discos.Disco;
 import com.github.britooo.looca.api.group.discos.DiscoGrupo;
 import com.github.britooo.looca.api.group.janelas.Janela;
 import com.github.britooo.looca.api.group.processos.Processo;
+import com.github.britooo.looca.api.util.Conversor;
 import dao.*;
 import modelo.Componente;
 import modelo.Computador;
@@ -12,11 +13,12 @@ import modelo.Monitoramento;
 import oshi.SystemInfo;
 import oshi.hardware.GraphicsCard;
 
+import java.awt.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import com.github.britooo.looca.api.util.Conversor;
 
-public class Howlz {
+public class HowlzLuiz {
     UsuarioDao usuarioDao = new UsuarioDao();
     ComputadorDao computadorDao = new ComputadorDao();
     ComponenteDao componenteDao = new ComponenteDao();
@@ -134,12 +136,42 @@ public class Howlz {
         janelaDao.salvar(janela);
     }
 
-    public Boolean estadoCritico() {
-        if (looca.getProcessador().getUso() > 90.0 || looca.getMemoria().getEmUso() > (looca.getMemoria().getTotal() - (looca.getMemoria().getTotal() / 10))) {
-            return true;
-        } else {
-            return false;
+    public List<String> estadoCritico() {
+        List<String> razoesEstadoCritico = new ArrayList<>();
+        // if (looca.getProcessador().getUso() > 90.0) {
+        if (looca.getProcessador().getUso() > 0.) {
+            razoesEstadoCritico.add("Uso da CPU acima de 90%");
         }
+        // if (looca.getMemoria().getEmUso() > (looca.getMemoria().getTotal() - (looca.getMemoria().getTotal() / 10))) {
+        if (looca.getMemoria().getEmUso() > (looca.getMemoria().getTotal() - (looca.getMemoria().getTotal()))) {
+            razoesEstadoCritico.add("Uso da Memória RAM acima de 90%");
+        }
+        return razoesEstadoCritico;
     }
 
+    // Projeto Individual do Luíz: Notificações de Alerta no Windows
+
+    // Instânciando SystemTray do Computador, para adicionar a aplicação à Barra de Tarefas:
+    SystemTray systemTray = SystemTray.getSystemTray();
+    Image imagem = Toolkit.getDefaultToolkit().createImage("owlHowlz.png");
+
+    TrayIcon trayIcon;
+    public void exibirNaBandeja() throws AWTException {
+        trayIcon = new TrayIcon(imagem, "Howlz");
+        trayIcon.setImageAutoSize(true);
+        systemTray.add(trayIcon);
+        // Adicionando aplicação à barra de tarefas:
+        trayIcon.displayMessage("Howlz Monitoramento", "Monitoramento Iniciado!", TrayIcon.MessageType.INFO);
+    }
+
+    public void emitirNotificacao(List<String> alertas) throws AWTException {
+        // Gerando mensagem de alerta dinamicamente, de acordo com a quantidade de alertas:
+        String mensagemAlerta = String.format("%d alerta(s):\n", alertas.size());
+        for (String alertaAtual : alertas) {
+            mensagemAlerta = String.format("%s%s\n", mensagemAlerta, alertaAtual);
+        }
+
+        // Exibindo notificação no Windows:
+        trayIcon.displayMessage("Howlz Monitoramento", mensagemAlerta, TrayIcon.MessageType.WARNING);
+    }
 }
