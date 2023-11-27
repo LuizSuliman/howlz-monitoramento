f53c2b8f62335b15b9d0606b2b36e1181ee6cea3
LuizSuliman <luiz.suliman@sptech.school>
2023-11-27 01:25:45 -0300

    Recriando arquivo .jar

--- src/main/java/servico/Slack.java
+++ src/main/java/servico/Slack.java
@@ -6,16 +6,6 @@ import java.io.IOException;
 
 public class Slack {
     public static void enviarMensagem(String mensagem) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"channel\": \"C065BU9GAQG\",\r\n  \"text\": \"%s\"\r\n}".formatted(mensagem));
-        Request request = new Request.Builder()
-                .url("https://slack.com/api/chat.postMessage")
-                .method("POST", body)
-                .addHeader("Content-type", "application/json")
-                .addHeader("Authorization", "Bearer ???") // REMOVER TOKEN ANTES DE SUBIR NO GIT
-                .build();
-        Response response = client.newCall(request).execute();
-        response.body().close();
+
     }
 }
